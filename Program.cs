07616c693473d7b1e7b657d2d811a27455b318e7
Cesar <cesarhenrique_prado@hotmail.com>
2025-05-13 16:33:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿string mensagemBoasVindas = "\nBoas Vindas ao Screen Sound!";
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 7});
+bandasRegistradas.Add("Pink Floyd", new List<int>());
+bandasRegistradas.Add("Metallica", new List<int>());
+
+void ExibirLogo() {
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░██╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗██║
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║╚═╝
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝██╗
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░╚═╝");
+    Console.WriteLine(mensagemBoasVindas);
+}
+
+void ExibirOpcoesMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumeria = int.Parse(opcaoEscolhida);
+
+
+    switch (opcaoEscolhidaNumeria)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2: MostrarTodasBandas();
+            break;
+        case 3: AvaliarBanda();
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumeria);
+            break;
+        case 0: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumeria);
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTituloMenu("Registro de bandas");
+    Console.Write("\nDigite o nome da banda que deseja cadastrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    bandasRegistradas.Add(nomeBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+
+void MostrarTodasBandas()
+{
+    Console.Clear();
+    ExibirTituloMenu("Lista de bandas cadastradas");
+    /*for (int i = 0; i < listaBandas.Count; i++)
+    {
+        Console.WriteLine($"Banda: {listaBandas[i]}");
+    }*/
+    foreach (string banda in bandasRegistradas.Keys)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine("\nPressione uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+
+void AvaliarBanda()
+{
+    Console.Clear();
+    ExibirTituloMenu("Avalie a banda");
+    Console.Write("Digite a banda que você gostaria de avaliar: ");
+    string nomeBanda = Console.ReadLine()!;
+    if( bandasRegistradas.Keys.Contains(nomeBanda))
+    {
+        Console.Write($"Qual nota a banda {nomeBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeBanda}.");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ExibirOpcoesMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeBanda} não foi cadastrada");
+        Console.WriteLine("Pressione uma tecla para voltar ao menu principal.");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesMenu();
+    }
+}
+
+void ExibirTituloMenu(string titulo)
+{
+    int quantidadeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+
+}
+
+ExibirOpcoesMenu();
